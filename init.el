2e637b644cab468f80a7a543655c635a07c71e68
cburst <aa@bb.cc>
2016-06-19 14:55:36 +0200

    initial commit

---
+++ init.el
@@ -0,0 +1,31 @@
+
+;; Package stuff
+(require 'package)
+(add-to-list 'package-archives
+             '("melpa" . "https://melpa.org/packages/"))
+(when (< emacs-major-version 24)
+  (add-to-list 'package-archives '("gnu" . "http://elpa.gnu.org/packages/")))
+
+; list the packages you want
+(setq package-list '(evil))
+
+; list the repositories containing them
+(setq package-archives '(("melpa" . "https://melpa.org/packages/")
+                         ("gnu" . "http://elpa.gnu.org/packages/")))
+
+; activate all the packages (in particular autoloads)
+(package-initialize)
+
+; fetch the list of packages available 
+(unless package-archive-contents
+  (package-refresh-contents))
+
+; install the missing packages
+(dolist (package package-list)
+  (unless (package-installed-p package)
+    (package-install package)))
+
+;; Package initializations
+(require 'evil)
+(evil-mode 1) 
+
