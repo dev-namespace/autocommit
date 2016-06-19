7bf34c53bc8e10146f143579c0066af28deb9820
esquembre <esquembre>
2016-06-20 01:16:56 +0200

    new plugins

--- init.el
+++ init.el
@@ -7,7 +7,7 @@
   (add-to-list 'package-archives '("gnu" . "http://elpa.gnu.org/packages/")))
 
 ; list the packages you want
-(setq package-list '(evil))
+(setq package-list '(evil auto-complete color-theme))
 
 ; list the repositories containing them
 (setq package-archives '(("melpa" . "https://melpa.org/packages/")
@@ -25,7 +25,17 @@
   (unless (package-installed-p package)
     (package-install package)))
 
-;; Package initializations
+;; ===== Package initializations =====
 (require 'evil)
 (evil-mode 1) 
 
+;;; auto complete mod
+;;; should be loaded after yasnippet so that they can work together
+(require 'auto-complete-config)
+(add-to-list 'ac-dictionary-directories "~/.emacs.d/ac-dict")
+(ac-config-default)
+;;; set the trigger key so that it can work together with yasnippet on tab key,
+;;; if the word exists in yasnippet, pressing tab will cause yasnippet to
+;;; activate, otherwise, auto-complete will
+(ac-set-trigger-key "TAB")
+(ac-set-trigger-key "<tab>")
